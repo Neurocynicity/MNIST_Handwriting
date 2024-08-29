00b63832f32386594e3ace7e75914da4ea276a67
Neurocynicity <105517800+Neurocynicity@users.noreply.github.com>
2024-08-29 20:12:25 +0100

    Made testing and training scripts

---
+++ Test.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Load the MNIST Dataset
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+# Build an evaluation pipeline
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+# Load the model
+model = tf.keras.models.load_model('MNIST_Handwriting_Model.keras')
+print(model.predict(ds_test).shape)
